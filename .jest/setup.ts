856b90da0c2c850fdbaabd0ba0f95aaf48c001a3
Marcio Nitao <marcionitao@gmail.com>
2022-07-22 18:02:06 +0200

    Initial commit from Create Next App

---
+++ .jest/setup.ts
@@ -0,0 +1,6 @@
+/**
+ * Aqui serão adicionados alguns imports de libs que iremos usar com o jest, por exemplo,
+ * recursos da testing-library, styled-components, etc.
+ */
+import '@testing-library/jest-dom'
+import 'jest-styled-components'
---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyle from '../styles/global'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092b" />
+        <meta
+          name="description"
+          content="A simple project started to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
+}
