86f756dc8d6915a94f22f8871804ecb375d44eb9
Marcio Nitao <marcionitao@gmail.com>
2022-07-25 09:28:02 +0200

    first commit

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,7 @@
-import * as S from './styles'
-
 export default function Main() {
   return (
-    <S.Wrapper>
-      <S.Logo
-        src="/img/logo.svg"
-        alt="Image de um átomo e React Avançado escrito ao lado"
-      />
-      <S.Title>Hello World!</S.Title>
-      <S.Description>
-        TypeScript, React, Next and Styled Components
-      </S.Description>
-      <S.Illustration
-        src="/img/hero-illustration.svg"
-        alt="Um desenvolvedor de frente para uma tela com código"
-      />
-    </S.Wrapper>
+    <div>
+      <h1>Mein Reise ✈</h1>
+    </div>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,14 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado</title>
+        <title>Mein Reise</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092b" />
         <meta
           name="description"
-          content="A simple project started to work with TypeScript, React, NextJS and Styled Components"
+          content="A simple project to show my favorite places in Germany and World."
         />
       </Head>
       <GlobalStyle />
